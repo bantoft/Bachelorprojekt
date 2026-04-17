5b90bd8310b28a02033aec870de33461f32e75d5
Andreas Antoft <Andreas@antoft.dk>
2026-04-17 15:26:00 +0200

    Lambda med mask og param fra log

--- SciML/core/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Core SciML package with models, operators and losses."""
-
-from .model import PINN
-from .params import load_params_from_bout_inp, resolve_hesel_params
-from .losses import pde_loss
